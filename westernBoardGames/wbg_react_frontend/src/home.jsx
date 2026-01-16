93168faae3be0915eaec7353d6a0f57cda29b546
Violetta Wojtas <vwojtas@uwo.ca>
2026-01-16 14:02:43 -0500

    Creating Home Page Base Files

---
+++ westernBoardGames/wbg_react_frontend/src/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './home.css';
+
+export default function Home() {
+    return (
+        <div className="home-page">
+            <h1>Welcome to Western Board Games</h1>
+        </div>
+    );
+}
