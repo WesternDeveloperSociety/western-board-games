5c139072f8be646d00363abe19397cca190af23f
ecmoyer <emily.moyer257@gmail.com>
2026-01-28 12:38:05 -0500

    commit changes before pulling updated dev branch

--- westernBoardGames/wbg_react_frontend/src/App.jsx
+++ westernBoardGames/wbg_react_frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Checkout from './pages/Checkout';
+import Profile from './pages/Profile';
 import Home from './pages/home';
 
 
 function App() {
   return (
-    <Home />
+    <Profile />
   );
 }
 
