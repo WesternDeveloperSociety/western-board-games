bc2dead2d8934f103baa9b9890fa0e687f5d9aa0
ecmoyer <emily.moyer257@gmail.com>
2026-02-22 20:26:39 -0500

    Add serializers for User and UserProfile

--- westernBoardGames/user/serializers.py
+++ westernBoardGames/user/serializers.py
@@ -1,1 +1,12 @@
 from rest_framework import serializers
+from .models import User, UserProfile
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "name", "email", "student_id"]
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ["id", "user", "is_active", "bio", "avatar", "active_rentals_available"]
