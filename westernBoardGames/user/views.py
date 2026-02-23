c6b4240131e86def4aecb7fef4a57b5c6885f752
ecmoyer <emily.moyer257@gmail.com>
2026-02-22 20:28:42 -0500

    Add views for User and UserProfile data (single user)

--- westernBoardGames/user/views.py
+++ westernBoardGames/user/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from .models import User, UserProfile
+from .serializers import UserSerializer, UserProfileSerializer
 
 # Create your views here.
+
+def GetUser(request):
+    user = User.objects.get(request.user)
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+def GetProfile(request):
+    user = UserProfile.objects.get(request.user)
+    serializer = UserProfileSerializer(user, many=False)
+    return Response(serializer.data)
